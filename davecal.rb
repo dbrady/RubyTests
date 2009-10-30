867e9bd8a424c7a683856638c6943640fb4b2004
David Brady <dbrady@DavePro.local>
2009-10-30 10:55:27 -0600

    Finished first cut at davecal.rb. Not OO but more rubyish. Needs to be wrapped into an object so we can split out the formatting functionality.

--- davecal.rb
+++ davecal.rb
@@ -6,12 +6,39 @@ today = Date.today
 
 month = today.month
 day = 1
-dow = today.wday
+dow = Date.new(today.year, today.month, 1).wday
 
-puts Date::MONTHNAMES[month] + " " + today.year
-puts "Su Mo Tu We Th Fr Sa"
-
-puts "   " * dow
-
-while 
+def format_bar(ticks=true)
+  if ticks
+    "+----" * 7 + "+"
+  else
+    "+" + '-' * 34 + "+"
+  end
 end
+
+def format_title(title)
+  len = 34
+  ws1 = ((len-title.length)/2) + title.length
+  ws2 = len - ws1
+  
+  "|%#{ws1}s%#{ws2}s|" % [title, '']
+end
+
+def format_row(row)
+  "|#{(0..6).map {|i| ' %02s ' % row[i]} * '|'}|"
+end
+
+title = "#{Date::MONTHNAMES[month]} #{today.year}"
+
+dates = [nil] * (dow-1) + (1..31).to_a
+
+
+puts format_bar(false)
+puts format_title(title)
+puts format_bar
+puts format_row(%w{Su Mo Tu We Th Fr Sa})
+puts format_bar
+dates.each_slice(7) do |row|
+  puts format_row(row)
+end
+puts format_bar
